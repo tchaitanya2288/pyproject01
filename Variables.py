1f83324781eb7fb713732989950184cbb59259da
tchaitanya2288 <tchaitanya.2288>
2018-05-08 11:38:37 -0400

    variables

---
+++ Variables.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+# Single Line Comments : # , ' ', " ", ''' ''', & """ """
+# Multiline Comments   : ''' ''', & """ """
+
+'''
+Name of the Prog : Variables
+Author           : KeshavKummari
+Env              : Development, Test, Acceptance & Production
+Platforms        : Windows, MacOS, Linux & Unix
+'''
+
+'Rule of Creating Variables in Python'
+"""
+1. A-Z
+2. a-z
+3. A-Za-z
+4. 0-9
+5. _ Special Character
+6. A-Za-z0-9_
+"""
+# Note : A variable name can not start with Integer Value(0-9)
+
+'Creating Variables in Python'
+FIRSTNAME  = 'Praveen'
+middlename = "Kumar"
+LastName   = 'Tumma'
+_Software  = 'Python'
+_007       = 3.6
+
+"Accessing Variables in Python"
+
+print(FIRSTNAME)
+print(middlename)
+print(LastName)
+print(_Software)
+print(_007)
