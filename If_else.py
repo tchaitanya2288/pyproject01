0a013ef33cbf09b028b1a3aca68d54e631ffbf8d
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-05-08 16:08:17 -0400

    IF_else

---
+++ If_else.py
@@ -0,0 +1,10 @@
+#2. IF..ELSE STATEMENT :
+
+var_1=50
+
+if var_1<100:
+    print (var_1, "is less than 100")
+else:
+    print ("I am Else Block",var_1,'is less than 100')
+
+#print ("We are out of the if..else block")
