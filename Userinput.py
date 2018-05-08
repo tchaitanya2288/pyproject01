dd1541034f9e48f034321407dc3ed7689aad8e66
tchaitanya2288 <tchaitanya.2288>
2018-05-08 14:49:11 -0400

    user input

---
+++ Userinput.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+name = input("What is your Name : ")
+
+print ("My name is : ", name)
+
+age = int(input("Enter your age: "))
+
+print("My age is",age)
+
+
+print(type(name),id(name))
+
+print(type(age),id(age))
