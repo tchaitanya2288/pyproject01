3dac52a2b0e24bdd6fe2d30fe79158a29483b18c
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-05-09 12:03:37 -0400

    Membership operators

---
+++ Membership Operators.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+#Membership operators in Python
+
+x = 'Welcome to Python'
+y = {1:'a',2:'b'}
+
+# Output: True
+
+print('h' in x)
+
+# Output: True
+print('hello' not in x)
+
+# Output: True
+print(1 in y)
+
+# Output: False
+print('a' in y)
