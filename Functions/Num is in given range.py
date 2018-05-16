be1ded08b26551baa87426c83437889478bd32d3
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-05-16 16:20:22 -0400

    num in range

---
+++ Functions/Num is in given range.py
@@ -0,0 +1,11 @@
+while True:
+   n = int(input("enter a number between 0 and 50: "))
+   if 0 <= n <= 50:
+      print('The num is in range')
+      break
+   else:
+       print("The num is not in range")
+       break
+  
+
+
