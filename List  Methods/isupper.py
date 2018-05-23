915922115955d06b14df0c863d3cce5dab3b7e73
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-05-23 13:37:30 -0400

    is_upper

---
+++ List  Methods/isupper.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+str = "THIS IS STRING EXAMPLE....WOW!!!"
+print (str.isupper())
+
+str = "THIS is string example....wow!!!"
+print (str.isupper())
+
+"""
+17	isupper()
+	Returns true if string has at least one cased character and all
+	cased characters are in uppercase and false otherwise.
+"""
