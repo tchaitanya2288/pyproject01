c97c6a437f6532785ec1afd487d93eb86c538644
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-05-23 13:45:54 -0400

    is_space

---
+++ List  Methods/is_space.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+_01_ = " "
+print (_01_.isspace())
+
+_02_ = "Linux Unix"
+print (_02_.isspace())
+
+_03_ = "Linux Unix Perl Python and django...etc!!!"
+print (_03_.isspace())
+
+"""
+15	isspace()
+	Returns true if string contains only whitespace
+	characters and false otherwise.
+"""
