8f63de0cf6fc46354d890ef43fc7271a9c0f7d7c
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-05-23 13:47:14 -0400

    is_lower

---
+++ List  Methods/is_lower.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+rossum = "python with django framework!"
+
+print (rossum.islower())
+
+rossum = "Linux Unix Perl Python and django...etc!!!"
+
+print (rossum.islower())
+
+"""
+13	islower()
+	Returns true if string has at least 1 cased character and all cased
+	characters are in lowercase and false otherwise.
+"""
