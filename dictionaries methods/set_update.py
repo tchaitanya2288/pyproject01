13f40bd2b4355dadc064a953ea9a05ad3ac1f426
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-05-24 12:13:04 -0400

    set_update

---
+++ dictionaries methods/set_update.py
@@ -0,0 +1,34 @@
+##6	Set Union with Mutation =		a_set.update()
+
+# Syntax: a_set.update(an_iter)
+
+# Example:
+
+s = set([1, 2, 3, 4, 5])
+s.update(set([5, 6, 7]))
+
+print(s)
+
+#Output : set([1, 2, 3, 4, 5, 6, 7])
+
+'''
+Mutates a_set to be the union of set a_set and the set of 
+elements in iterable an_iter. Returns None.
+'''
+
+
+#Set Union with Mutation = a_set.update()
+
+# Syntax: a_set.update(an_iter)
+
+# Example:
+
+s = set([1, 2, 3, 4, 5])
+s.update(set([5, 6, 7]))
+
+print(s)
+
+#Output : set([1, 2, 3, 4, 5, 6, 7])
+
+'''Mutates a_set to be the union of set a_set and the set of 
+elements in iterable an_iter. Returns None.'''
