9cb12568023c902a45a5751b3ed48a5a3f955427
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-05-24 12:50:46 -0400

    set_symmetricdiff

---
+++ dictionaries methods/set_symmetricdifference.py
@@ -0,0 +1,11 @@
+#5	Set Symmetric Difference =		a_set.symmetric_difference()
+
+# Syntax: a_set.symmetric_difference(anInter)
+
+# Example:
+
+print(set([1, 2, 3, 4, 5,6]).symmetric_difference(set([5, 6, 7])))
+#Output : set([1, 2, 3, 4, 6, 7])
+
+'''Returns a set with all elements that are in exactly one of set a_set and 
+iterable an_iter.'''
