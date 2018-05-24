81feabef5ac41625243b577d0e857265cb6127dc
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-05-24 12:42:43 -0400

    set_union

---
+++ dictionaries methods/set_union.py
@@ -0,0 +1,14 @@
+
+#2	Set Union  	=		a_set.union()
+# Syntax: a_set.union(an_iter)
+# Example:
+#print (set([1, 2, 3, 4, 5]).union(set([5, 6, 7])))
+
+#Output : set([1, 2, 3, 4, 5, 6, 7])
+
+#Returns the union of set a_set and the set of
+#elements in iterable an_iter.
+
+
+print (set([1, 2, 3, 4, 5,6]).union(set([5, 6, 7,8,9])))
+
