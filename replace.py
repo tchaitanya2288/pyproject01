face255bab737aab2c976be3dc3a13c7854f9013
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-05-28 12:57:21 -0400

    replace

---
+++ replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+str = "python is scripting is and is programming is language"
+print (str.replace("is", "was"))
+print (str.replace("is", "was", 3))
+
+
+"""
+26	replace(old, new [, max])
+	Replaces all occurrences of old in string with new or at
+	most max occurrences if max given.
+"""
