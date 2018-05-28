105bb509d9ae3a1a38adc7cc96f269ea93541ed7
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-05-28 12:44:31 -0400

    set_intersection

---
+++ Sets_Methods/set_intersection.py
@@ -0,0 +1,10 @@
+#3	Set Intersection  =	a_set.intersection()
+
+# Syntax: a_set.intersection(an_iter)
+
+# Example:
+
+print(set([1, 2, 3, 4, 5]).intersection(set([5, 6, 7])))
+#Output : set([5])
+
+'''Returns the intersection of set a_set and the set of elements in iterable an_iter.'''
