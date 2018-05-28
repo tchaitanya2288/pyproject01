807938e82b8379278db0528fda2e8220d7250f27
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-05-28 12:35:40 -0400

    set_pop

---
+++ dictionaries methods/set_pop.py
@@ -0,0 +1,16 @@
+# 12	Remove Arbitrary Element from Set           =       a_set.pop()
+
+# Syntax: a_set.pop()
+
+# Example:
+
+s = set([3,4,5,6])
+print(s.pop())
+print(s)
+
+# Output :
+# 1
+# set([2, 3, 4, 5])
+
+'''Removes and returns an arbitrary element from set a_set. 
+Raises an error if there are no elements to remove.'''
