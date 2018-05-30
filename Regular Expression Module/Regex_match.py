4c50b8d13bbfcdd69614cf4fda0f9d77d27fc2c5
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-05-30 12:16:31 -0400

    regex_match

--- Regular Expression Module/Regex_match.py
+++ Regular Expression Module/Regex_match.py
@@ -1,13 +1,13 @@
 #!usr\bin\python
 import re
 
-str ='python and perl are programming languahes'
+line = "Python and perl are programming languahes"
 
-matchobj = re.match(r'(.*) are',str,re.M|re.I)
+matchObj = re.match(r'(.*) and',line,re.M|re.I)
 
-if matchobj:
-    print('matchobj.group():',matchobj.group)
-    print('matchobj.groups()',matchobj.groups)
+if matchObj:
+    print("matchObj.group(): ",matchObj.group)
+    print("matchObj.groups() ",matchObj.groups)
 
 else:
     print("match not found")
