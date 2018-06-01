d8983bbcd6bfb2a27b9cabd5079b57bcd026fc03
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-06-01 16:01:28 -0400

    Create_file

---
+++ File_creation/create_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import os
+
+os.getcwd()
+
+os.listdir()
+
+
+filecreate = open("test_file.txt","w")
+
+filecreate.write("welcome to python world")
+'''
+filecreate = open("test_file.txt","rt")
+
+filecreate.read()
+
+filecreate.close()
+'''
+
