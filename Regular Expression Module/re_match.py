a7e1fa372d8ad5862a002e959bdb163e12af651d
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-06-01 12:47:10 -0400

    reg_ex match

--- Regular Expression Module/re_match.py
+++ Regular Expression Module/re_match.py
@@ -2,9 +2,9 @@
 
 import re
 
-line = "python and perl are languages"
-
-matchObj = (r'(.*) and', line , re.M | re.I)
+line = "python and perl are languages those are easy"
+#(.) means any character in the string,(.*) everything in the line
+matchObj = re.match(r'(.*) are(.*)', line , re.M | re.I)
 
 if matchObj:
     print("matchObj.group() :", matchObj.group())
