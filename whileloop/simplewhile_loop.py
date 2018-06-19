565660b73039db6f0e9ed986504c2f96ba674f9c
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-06-19 14:44:47 -0400

    simple_whileloop

---
+++ whileloop/simplewhile_loop.py
@@ -0,0 +1,10 @@
+#!/usr/bin/data
+
+while True:
+    num=input('Enter a Number : ')
+    var=str(num)
+    if(ord(var) in range(48,58)):  #end -1 58-1 : 57
+        break
+print ("We are out of the while loop")
+
+
